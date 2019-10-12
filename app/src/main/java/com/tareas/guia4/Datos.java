8cbd28a5f9d29944dd4a5717139b43f44ed9bac7
csolis7 <cesarssolis7@gmail.com>
2019-10-11 22:59:04 -0600

    Initial commit

---
+++ app/src/main/java/com/tareas/guia4/Datos.java
@@ -0,0 +1,14 @@
+package com.tareas.guia4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+    }
+}
